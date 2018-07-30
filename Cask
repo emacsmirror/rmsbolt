2e6163c95a118db9c10e20a2bcc0ee0adab47a1b
Jay Kamat <jaygkamat@gmail.com>
2018-07-29 18:05:50 -0700

    Add testing framework

---
+++ Cask
@@ -0,0 +1,10 @@
+(source gnu)
+(source melpa)
+
+(package-file "rmsbolt.el")
+
+(files "rmsbolt.el")
+
+(development
+ (depends-on "ert-runner")
+ (depends-on "el-mock"))
---
+++ test/rmsbolt-test.el
@@ -0,0 +1,12 @@
+;;; rmsbolt-test.el --- Tests for rmsbolt
+
+;;; Commentary:
+;; Tests for rmsbolt
+
+;;; Code:
+
+(ert-deftest sanity-check-ert ()
+  "Check if ERT is working. :)"
+  (should t))
+
+;;; rmsbolt-test.el ends here
